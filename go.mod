fda91446f476fd9f897fd32086ca99d589c3984f
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-12-25 04:00:00 +0700

    test(service/service_test.go): login service
    create test for login service with three test cases. Also refactor some, well this commit was wrong
    T-T

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1
 	github.com/joho/godotenv v1.3.0
+	github.com/securego/gosec/v2 v2.5.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
--- service/service_test.go
+++ service/service_test.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
 
@@ -16,18 +15,23 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
+var server *Server
+
 func init() {
-	client, coll, mongoErr := mongodb.NewMongoDBConnection(os.Getenv("MONGO_DB_USER"), os.Getenv("MONGO_DB_PASS"), os.Getenv("MONGO_DB"), os.Getenv("MONGO_DB_COLLECTION"))
+	_, coll, mongoErr := mongodb.NewMongoDBConnection(os.Getenv("MONGO_DB_USER"), os.Getenv("MONGO_DB_PASS"), os.Getenv("MONGO_DB"), os.Getenv("MONGO_DB_COLLECTION"))
 	if mongoErr != nil {
 		panic(mongoErr)
 	}
-	defer client.Disconnect(context.TODO())
-
 	coll.DeleteMany(context.TODO(), bson.M{"email": "Testing@mail.com"})
+
+	tokenService := jwt.NewJWTService(string(os.Getenv("JWT_SECRET")), string(os.Getenv("APP_NAME")))
+	userRepo := user.NewMongoDBRepository(coll)
+	userManager := user.NewManager(userRepo)
+	server = NewService(os.Getenv("SERVER_NETWORK"), os.Getenv("SERVER_ADDRESS"), userManager, tokenService)
+
 }
 
 func TestRegisterNewUser(t *testing.T) {
-	t.Parallel()
 	testCases := []struct {
 		name    string
 		code    codes.Code
@@ -61,17 +65,7 @@ func TestRegisterNewUser(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 
-			_, coll, mongoErr := mongodb.NewMongoDBConnection(os.Getenv("MONGO_DB_USER"), os.Getenv("MONGO_DB_PASS"), os.Getenv("MONGO_DB"), os.Getenv("MONGO_DB_COLLECTION"))
-			if mongoErr != nil {
-				panic(mongoErr)
-			}
-
-			tokenService := jwt.NewJWTService(string(os.Getenv("JWT_SECRET")), string(os.Getenv("APP_NAME")))
-			userRepo := user.NewMongoDBRepository(coll)
-			userManager := user.NewManager(userRepo)
-			authServer := NewService(os.Getenv("SERVER_NETWORK"), os.Getenv("SERVER_ADDRESS"), userManager, tokenService)
-
-			res, err := authServer.RegisterService(context.TODO(), &authpb.RegisterRequest{
+			res, err := server.RegisterService(context.TODO(), &authpb.RegisterRequest{
 				Fullname: tc.payload.GetFullname(),
 				Email:    tc.payload.GetEmail(),
 				Password: tc.payload.GetPassword(),
@@ -82,7 +76,58 @@ func TestRegisterNewUser(t *testing.T) {
 				assert.NotNil(t, res.GetAccessToken(), "Access token should not be empty")
 				assert.Equal(t, res.GetMessage(), "Register success", "Message should be equal to 'Register Success'")
 			} else {
-				fmt.Println("Error ->", err.Error())
+				assert.NotNil(t, err, "Error should be not nil")
+				assert.Nil(t, res, "Response should be nil")
+			}
+		})
+	}
+}
+
+func TestLoginService(t *testing.T) {
+	_, _ = server.RegisterService(context.TODO(), &authpb.RegisterRequest{
+		Fullname: "User Testing",
+		Email:    "Testing@mail.com",
+		Password: "PasswordTest.com",
+	})
+	testCases := []struct {
+		name    string
+		payload *authpb.LoginRequest
+		code    codes.Code
+	}{
+		{
+			name: "Login Success",
+			code: codes.OK,
+			payload: &authpb.LoginRequest{
+				Email:    "Testing@mail.com",
+				Password: "PasswordTest.com",
+			},
+		},
+		{
+			name: "Login Failed Due To Wrong Credential",
+			code: codes.Unauthenticated,
+			payload: &authpb.LoginRequest{
+				Email:    "Testing@mail.com",
+				Password: "PasswordTest",
+			},
+		},
+		{
+			name: "Login Failed Due To User Does Not Exist",
+			code: codes.Unauthenticated,
+			payload: &authpb.LoginRequest{
+				Email:    "emangada@mail.com",
+				Password: "PasswordTest.com",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := server.LoginService(context.TODO(), tc.payload)
+			if tc.code == codes.OK {
+				assert.Nil(t, err, "Error should be empty")
+				assert.NotNil(t, res.GetAccessToken(), "Access token should not be empty")
+				assert.Equal(t, res.GetMessage(), "Login Success", "Message should be equal to 'Login Success'")
+			} else {
 				assert.NotNil(t, err, "Error should be not nil")
 				assert.Nil(t, res, "Response should be nil")
 			}
